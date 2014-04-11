a4fc573bfd31d091907f1fde8db862b7978511b8
Mahopa <mahopa@gmail.com>
2014-04-11 11:51:45 -0500

    Initial

---
+++ Polling/_poll.js
@@ -0,0 +1,19 @@
+﻿var myApp = {};
+myApp.WriteOutput = function () { };
+myApp.GetMessages = function () { };
+myApp.PostMessage = function () {
+    var request = new XMLHttpRequest();
+    request.open("POST",
+        "https://domo.firebaseio.com/.json",
+        true);
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            myApp.Poll();
+        }
+    }
+    request.send(JSON.stringify("Stamp: " + Date.now()));
+};
+myApp.Poll = function () {
+    // myApp.interval = setInterval(myApp.PostMessage, 3000);
+    setTimeout(function () { myApp.PostMessage(); }, 3000);
+};
